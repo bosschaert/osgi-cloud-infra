cb01a60d86a5604d725c2670af61983bd97e475d
David Bosschaert <david.bosschaert@gmail.com>
2012-06-27 13:16:04 +0100

    Fix typo

--- disco-tunnel/create-tunnel-template.sh
+++ disco-tunnel/create-tunnel-template.sh
@@ -3,4 +3,4 @@ DISCOVERY_VM=... specify Discovery VM SSH login here, something like blahblahbla
 chmod 600 disco_id_rsa
 REMOTE_GEAR_IP=`ssh -q -o "StrictHostKeyChecking no" -i ./disco_id_rsa $DISCOVERY_VM printenv OPENSHIFT_INTERNAL_IP`
 echo REMOTE GEAR IP: $REMOTE_GEAR_IP
-ssh -q -g -o "StrictHostKeyChecking no" -i ./disco_id_rsa -N -L $OPENSHIFT_INTERNAL_IP:21810:127.3.241.1:21810 $DISCOVERY_VM
+ssh -q -g -o "StrictHostKeyChecking no" -i ./disco_id_rsa -N -L $OPENSHIFT_INTERNAL_IP:21810:$REMOTE_GEAR_IP:21810 $DISCOVERY_VM
