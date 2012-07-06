91c13de1afd2aeba0797e57a364e05c5652838b1
David Bosschaert <david.bosschaert@gmail.com>
2012-07-06 14:01:04 +0100

    Small tweak

--- osgi/equinox/launch-osgi-local.sh
+++ osgi/equinox/launch-osgi-local.sh
@@ -1,4 +1,4 @@
-export OPENSHIFT_GEAR_DNS=localhost
+export OPENSHIFT_GEAR_DNS=0.0.0.0
 export OPENSHIFT_INTERNAL_IP=127.0.0.1
 mkdir config-local
 # Remove the zookeeper-plugin as no port translation is needed for local runs
@@ -7,5 +7,5 @@ sed "s/^.*zookeeper-plugin.*$/\\\\/" < config/config.ini > config-local/config.i
 mkdir load
 cp load-template/org.apache.cxf.dosgi.discovery.zookeeper.cfg load
 echo zookeeper.host = $OPENSHIFT_INTERNAL_IP >> load/org.apache.cxf.dosgi.discovery.zookeeper.cfg
-java -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_INTERNAL_IP -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config-local -clean 
+java -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_GEAR_DNR -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config-local -clean 
 
