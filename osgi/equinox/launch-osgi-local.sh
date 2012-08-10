102b7098f1260469094f5c32fe27a088b103ae95
David Bosschaert <david.bosschaert@gmail.com>
2012-08-10 10:21:24 +0100

    Remote Provisioning

--- osgi/equinox/launch-osgi-local.sh
+++ osgi/equinox/launch-osgi-local.sh
@@ -15,5 +15,5 @@ else
     JAVA_OPTS=""
 fi
 
-java $JAVA_OPTS -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_GEAR_DNS -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config-local -clean 
+java $JAVA_OPTS -Dcom.sun.management.jmxremote.port=29999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_GEAR_DNS -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config-local -clean 
 
--- osgi/equinox/launch-osgi.sh
+++ osgi/equinox/launch-osgi.sh
@@ -1,4 +1,4 @@
 mkdir load
 cp load-template/org.apache.cxf.dosgi.discovery.zookeeper.cfg load
 echo zookeeper.host = $OPENSHIFT_INTERNAL_IP >> load/org.apache.cxf.dosgi.discovery.zookeeper.cfg
-java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_INTERNAL_IP -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config -clean 
+java -Dcom.sun.management.jmxremote.port=29999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dorg.ops4j.pax.web.listening.addresses=$OPENSHIFT_INTERNAL_IP -jar org.eclipse.osgi_3.7.2.v20120110-1415.jar -configuration config -clean 
